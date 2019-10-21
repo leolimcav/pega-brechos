8c65d75369508e8e7ddd98f885b285d07d5e075c
Leonardo <leonardo123k@gmail.com>
2019-10-21 10:38:33 -0300

    atualizado model de usuario

--- src/models/Users.js
+++ src/models/Users.js
@@ -1,15 +1,6 @@
 const pool = require('../config/database');
 
 class Users {
-  // constructor(id, nome, email, endereco, nota, telefone) {
-  //   this.id = id,
-  //   this.nome = nome,
-  //   this.email = email,
-  //   this.endereco = endereco,
-  //   this.nota = nota,
-  //   this.telefone = telefone,
-  // }
-
   static async create(data) {
     const { nome, email, senha } = data;
     const client = await pool.connect();
