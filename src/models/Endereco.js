eac7e0cc400cc106637d95d151126702fbeaf983
Leonardo <leonardo123k@gmail.com>
2019-10-24 17:08:23 -0300

    add findByAndUpdate and findByIdAndDelete address model function

--- src/models/Endereco.js
+++ src/models/Endereco.js
@@ -27,6 +27,34 @@ class Endereco {
     await client.release();
     return endereco;
   }
+
+  static async findByIdAndUpdate(cep, data) {
+    const client = await pool.client();
+    const { rows: endereco } = await client.query('SELECT * FROM endereco WHERE cep = $1', [cep]);
+    if (endereco) {
+      const {
+        rua, bairro, cidade, uf, numero, complemento,
+      } = data;
+      const { rows: novoEndereco } = await client.query(
+        'UPDATE endereco set rua = $1, bairro = $2, cidade = $3, uf = $4, numero = $5, complemento = $6 RETURNING *',
+        [rua, bairro, cidade, uf, numero, complemento],
+      );
+      await client.release();
+      return novoEndereco;
+    }
+    return endereco;
+  }
+
+  static async findByIdAndDelete(cep) {
+    const client = await pool.connect();
+    const { rows: endereco } = await client.query('SELECT * FROM endereco WHERE cep = $1', [cep]);
+    if (endereco) {
+      await client.query('DELETE FROM endereco WHERE cep = $1', [cep]);
+      await client.release();
+      return endereco;
+    }
+    return endereco;
+  }
 }
 
 module.exports = Endereco;
