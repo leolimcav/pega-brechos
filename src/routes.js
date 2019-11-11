5c4f71358bca72626c8a1ab0cbc13a03689d38b5
Michelle Romão <michelle.nunes10@gmail.com>
2019-11-11 20:47:25 -0300

    atualizando

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,10 @@
-const { Router } = require('express');
-const UserController = require('./controllers/UserController');
+const { Router } = require("express");
+const UserController = require("./controllers/UserController");
 
 const routes = Router();
 
-routes.post('/users', UserController.store);
-routes.get('/users/:userid', UserController.findById);
-routes.get('/users', UserController.index);
+routes.post("/users", UserController.store);
+routes.get("/users/:userid", UserController.findById);
+routes.get("/users", UserController.index);
 
 module.exports = routes;
