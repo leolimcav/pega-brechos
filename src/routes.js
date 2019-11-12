2db240fa8dc8cc2dae2217903ad405dceb4e3e5e
Michelle Romão <michelle.nunes10@gmail.com>
2019-11-11 21:15:06 -0300

    att

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,15 @@
-const UserController = require("./controllers/UserController");
+const express = require("express");
+const UsuarioController = require("./controllers/UsuarioController");
 
-routes.get("/users/:userid", UserController.findById);
-routes.get("/users", UserController.index);
+const routes = express.Router();
 
-routes.get("/", UsuarioController.index);
+routes.get("/users/:userid", UsuarioController.findById);
+routes.get("/users", UsuarioController.index);
+
+// routes.get("/", UsuarioController.index);
+routes.get("/", (req, res) => {
+  res.render("index");
+});
 
 routes.post("/users", UsuarioController.store);
 routes.get("/users/:userid", UsuarioController.findById);
