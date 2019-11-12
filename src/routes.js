668b26939cac06b1c32cef6844bc19b601674835
Leonardo <leonardo123k@gmail.com>
2019-11-11 21:14:07 -0300

    static files config set

--- src/routes.js
+++ src/routes.js
@@ -3,7 +3,10 @@ const UsuarioController = require('./controllers/UsuarioController');
 
 const routes = express.Router();
 
-routes.get('/', UsuarioController.index);
+// routes.get('/', UsuarioController.index);
+routes.get('/', (req, res) => {
+  res.render('index');
+});
 
 routes.post('/users', UsuarioController.store);
 routes.get('/users/:userid', UsuarioController.findById);
--- src/server.js
+++ src/server.js
@@ -7,6 +7,7 @@ const app = express();
 app.set('views', path.resolve(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use(express.static(path.resolve(__dirname, 'views')));
 app.use(express.json());
 app.use(routes);
 
