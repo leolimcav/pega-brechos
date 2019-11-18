3bc341050455f1c493f2d56feb080edea216c105
Michelle Romão <michelle.nunes10@gmail.com>
2019-11-18 03:06:47 -0300

    index responsive

--- src/routes.js
+++ src/routes.js
@@ -1,22 +1,22 @@
-const express = require("express");
-const UsuarioController = require("./controllers/UsuarioController");
+const express = require('express');
+const UsuarioController = require('./controllers/UsuarioController');
 
 const routes = express.Router();
 
-routes.get("/users/:userid", UsuarioController.findById);
-routes.get("/users", UsuarioController.index);
+routes.get('/users/:userid', UsuarioController.findById);
+routes.get('/users', UsuarioController.index);
 // routes.get('/', UsuarioController.index);
-routes.get("/", (req, res) => {
-  res.render("index");
+routes.get('/', (req, res) => {
+  res.render('index');
 });
 
 // routes.get("/", UsuarioController.index);
-routes.get("/", (req, res) => {
-  res.render("index");
+routes.get('/', (req, res) => {
+  res.render('index');
 });
 
-routes.post("/users", UsuarioController.store);
-routes.get("/users/:userid", UsuarioController.findById);
-routes.get("/users", UsuarioController.index);
+routes.post('/users', UsuarioController.store);
+routes.get('/users/:userid', UsuarioController.findById);
+routes.get('/users', UsuarioController.index);
 
 module.exports = routes;
---
+++ src/views/js/openMenu.js
@@ -0,0 +1,7 @@
+function openNav() {
+  document.getElementById('mySideNav').style.display = 'block';
+  document.getElementById('mySideNav').style.display = 'flex';
+}
+function closeNav() {
+  document.getElementById('mySideNav').style.display = 'none';
+}
