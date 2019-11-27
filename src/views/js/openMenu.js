07dfc9c71154aa97708451b8ab978aa35e2725e0
Michelle Romão <michelle.nunes10@gmail.com>
2019-11-26 21:09:58 -0300

    submenu user

--- src/views/js/openMenu.js
+++ src/views/js/openMenu.js
@@ -1,7 +1,25 @@
+document.getElementById("submenuUser").style.display = "none";
+
 function openNav() {
-  document.getElementById('mySideNav').style.display = 'block';
-  document.getElementById('mySideNav').style.display = 'flex';
+  document.getElementById("mySideNav").style.display = "block";
+  document.getElementById("mySideNav").style.display = "flex";
 }
 function closeNav() {
-  document.getElementById('mySideNav').style.display = 'none';
+  document.getElementById("mySideNav").style.display = "none";
+}
+
+function openSubmenu() {
+  const user = JSON.parse(localStorage.getItem("currentUser"));
+  if (user) {
+    console.log(user.username);
+    document.getElementById("submenuUser").style.display = "block";
+    document.getElementById("submenuUser").style.display = "flex";
+    document.getElementById("submenuUser").style.alignContent = "flex-end";
+  } else {
+    document.getElementById("submenuUser").style.display = "none";
+  }
+}
+
+function closeSubmenu() {
+  document.getElementById("submenuUser").style.display = "none";
 }
