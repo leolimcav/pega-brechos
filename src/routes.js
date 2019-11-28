d571b20256e55ae09a657eb37719fe920bf75912
Leonardo <leonardo123k@gmail.com>
2019-11-27 22:35:51 -0300

    atualizado rotas de usuario e model de usuario

--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,12 @@
 const express = require("express");
 const UsuarioController = require("./controllers/UsuarioController");
-const PessoaFisicaController = require("./controllers/PessoaFisicaController");
-const PessoaJuridicaController = require("./controllers/PessoaJuridicaController");
 const ProdutoController = require("./controllers/ProdutoController");
 const PedidoController = require("./controllers/PedidoController");
 const CarrinhoController = require("./controllers/CarrinhoController");
 
 const routes = express.Router();
 
-routes.get("/users/:userid", UsuarioController.findById);
-routes.get("/users", UsuarioController.index);
-// routes.get('/', UsuarioController.index);
+// Rotas do FRONTEND
 routes.get("/", (req, res) => {
   res.render("index");
 });
@@ -34,27 +30,19 @@ routes.get("/search", (req, res) => {
   res.render("search");
 });
 
+// ----- Rotas do Backend -----
+
 // Rotas de Usuario
+routes.get("/users/:user_id", UsuarioController.index);
 routes.post("/users", UsuarioController.store);
-routes.get("/users/:userid", UsuarioController.findById);
-routes.get("/users", UsuarioController.index);
-
-// Rotas de Usuario PF
-routes.get("/pf/:pfid", PessoaFisicaController.index);
-routes.post("/pf/:userid", PessoaFisicaController.store);
-routes.put("/pf/:pfid", PessoaFisicaController.update);
-routes.delete("/pf/:pfid", PessoaFisicaController.destroy);
-
-// Rotas de Usuario PJ
-routes.get("/pj/:pjid", PessoaJuridicaController.index);
-routes.post("/pj/:userid", PessoaJuridicaController.store);
-routes.put("/pj/:pjid", PessoaJuridicaController.update);
+routes.put("/users/:user_id", UsuarioController.update);
+routes.delete("/users/:user_id", UsuarioController.destroy);
 
 // Rotas de Produto
 routes.get("/products/:product_id", ProdutoController.index);
 routes.post("/products/:user_id", ProdutoController.store);
-routes.get("/products/:product_id", ProdutoController.update);
-routes.get("/products/:product_id", ProdutoController.destroy);
+routes.put("/products/:product_id", ProdutoController.update);
+routes.delete("/products/:product_id", ProdutoController.destroy);
 
 // Rotas de Pedido
 routes.get("/orders/:order_id/:user_id", PedidoController.index);
