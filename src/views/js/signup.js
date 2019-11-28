f9215d344311c61a0503f197bba2f45309d2fc54
Michelle Romão <michelle.nunes10@gmail.com>
2019-11-28 09:02:04 -0300

    mais tentativas em vao

--- src/views/js/signup.js
+++ src/views/js/signup.js
@@ -4,14 +4,21 @@ $("#cadastrar").submit(function(event) {
 
   // Get some values from elements on the page:
   const $form = $(this);
-  const nome = $form.find("input[id='nome']");
-  const email = $form.find("input[id='email']");
-  const senha = $form.find("input[id='senha']");
-  const cpf = $form.find("input[id='cpf']");
-  const rg = $form.find("input[id='rg']");
-  const date = $form.find("input[id='date']");
-  const sex = $form.find("input[name='sex']");
-  const telefone = $form.find("input[id='telefone']");
+  const nome = document.getElementById("nome");
+  const email = document.getElementById("email");
+  const senha = document.getElementById("pwd");
+  const cpf = document.getElementById("cpf");
+  const rg = document.getElementById("rg");
+  const date = document.getElementById("date");
+  const sex = document.getElementsByName("sex");
+  const telefone = document.getElementById("telefone");
+
+  if (sex.value === undefined) {
+    sex.value = "null";
+  }
+  if (telefone.value === "") {
+    telefone.value = "null";
+  }
 
   console.log(
     nome.value,
@@ -26,14 +33,14 @@ $("#cadastrar").submit(function(event) {
 
   // Send the data using post
   const posting = $.post("/users", {
-    nome: nome.value,
-    email: email.value,
-    senha: senha.value,
-    telefone: telefone.value,
-    rg: rg.value,
-    cpf: cpf.value,
-    date: date.value,
-    sex: sex.value
+    nome: `${nome.value}`,
+    email: `${email.value}`,
+    senha: `${senha.value}`,
+    telefone: `${telefone.value}`,
+    rg: `${rg.value}`,
+    cpf: `${cpf.value}`,
+    date: `${date.value}`,
+    sex: `${sex.value}`
   });
 
   // Put the results in a div
